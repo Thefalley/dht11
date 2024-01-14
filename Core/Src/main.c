1678738e01fd21b563310b9b64b5aa1f54f2519e
Thefalley <118454027+Thefalley@users.noreply.github.com>
2024-01-14 19:02:41 +0100

    Update main.c
    solo se lee bien el valor en la primera iteración

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,6 +24,9 @@
 /* USER CODE BEGIN Includes */
 #include <unistd.h>
 #include "dht11.h"
+#include "FreeRTOS.h"
+#include "timers.h"
+#include "semphr.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -42,8 +45,10 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim10;
 TIM_HandleTypeDef htim11;
 
+UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* Definitions for defaultTask */
@@ -67,28 +72,67 @@ const osThreadAttr_t myTask03_attributes = {
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityLow,
 };
+/* Definitions for myTask04 */
+osThreadId_t myTask04Handle;
+const osThreadAttr_t myTask04_attributes = {
+  .name = "myTask04",
+  .stack_size = 1024 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for myTask05 */
+osThreadId_t myTask05Handle;
+const osThreadAttr_t myTask05_attributes = {
+  .name = "myTask05",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for myTask06 */
+osThreadId_t myTask06Handle;
+const osThreadAttr_t myTask06_attributes = {
+  .name = "myTask06",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for myTask07 */
+osThreadId_t myTask07Handle;
+const osThreadAttr_t myTask07_attributes = {
+  .name = "myTask07",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
 /* USER CODE BEGIN PV */
-uint64_t count_timer = 0 ;
 osEventFlagsId_t flag_Event;
+uint8_t mode = 0;
+SemaphoreHandle_t delaySemaphore;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_TIM10_Init(void);
 static void MX_TIM11_Init(void);
 void StartDefaultTask(void *argument);
 void StartTask02(void *argument);
 void StartTask03(void *argument);
+void StartTask04(void *argument);
+void StartTask05(void *argument);
+void StartTask06(void *argument);
+void StartTask07(void *argument);
 
 /* USER CODE BEGIN PFP */
-void delay_f (uint32_t uSeg);
-
+void DHT11_Delay_f(uint32_t uSeg);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+	mode ++;
+	if(mode == 3){
+		mode = 0;
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -119,7 +163,9 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_USART1_UART_Init();
   MX_USART2_UART_Init();
+  MX_TIM10_Init();
   MX_TIM11_Init();
   /* USER CODE BEGIN 2 */
 
@@ -154,6 +200,18 @@ int main(void)
   /* creation of myTask03 */
   myTask03Handle = osThreadNew(StartTask03, NULL, &myTask03_attributes);
 
+  /* creation of myTask04 */
+  myTask04Handle = osThreadNew(StartTask04, NULL, &myTask04_attributes);
+
+  /* creation of myTask05 */
+  myTask05Handle = osThreadNew(StartTask05, NULL, &myTask05_attributes);
+
+  /* creation of myTask06 */
+  myTask06Handle = osThreadNew(StartTask06, NULL, &myTask06_attributes);
+
+  /* creation of myTask07 */
+  myTask07Handle = osThreadNew(StartTask07, NULL, &myTask07_attributes);
+
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -170,6 +228,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -194,14 +253,13 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 16;
   RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -224,6 +282,37 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief TIM10 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM10_Init(void)
+{
+
+  /* USER CODE BEGIN TIM10_Init 0 */
+
+  /* USER CODE END TIM10_Init 0 */
+
+  /* USER CODE BEGIN TIM10_Init 1 */
+
+  /* USER CODE END TIM10_Init 1 */
+  htim10.Instance = TIM10;
+  htim10.Init.Prescaler = 84-1;
+  htim10.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim10.Init.Period = 65000-1;
+  htim10.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim10.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim10) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM10_Init 2 */
+
+  /* USER CODE END TIM10_Init 2 */
+
+}
+
+/**
   * @brief TIM11 Initialization Function
   * @param None
   * @retval None
@@ -239,9 +328,9 @@ static void MX_TIM11_Init(void)
 
   /* USER CODE END TIM11_Init 1 */
   htim11.Instance = TIM11;
-  htim11.Init.Prescaler = 2000 - 1 ;
+  htim11.Init.Prescaler = 1-1;
   htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim11.Init.Period = 42000 - 1;
+  htim11.Init.Period = 84-1;
   htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim11) != HAL_OK)
@@ -251,6 +340,39 @@ static void MX_TIM11_Init(void)
   /* USER CODE BEGIN TIM11_Init 2 */
 
   /* USER CODE END TIM11_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
 
 }
 
@@ -316,6 +438,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PC0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
   /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -327,28 +455,48 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = DHT11_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(DHT11_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef * huart1){
 
-	if (htim == &htim11){
-
-		osEventFlagsSet(flag_Event, 0x01);
-
-	}
+	//HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 
 }
 
-void delay_f (uint32_t uSeg){
-	htim11.Init.Period = 84 - 1; // no cabiar (1us con el prescaler de 1)
-	// Maximo valor de 65536
-	htim11.Init.Prescaler = uSeg - 1  ;
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef * huart1){
+	//HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+}
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	if (htim == &htim11){
+		osEventFlagsSet(flag_Event, 0x01);
+		HAL_TIM_Base_Stop_IT(&htim11);
+	}
+}
+
+void DHT11_Delay_f(uint32_t uSeg) {
+
+	 htim11.Instance = TIM11;
+	  htim11.Init.Prescaler = (uSeg - 44) -1;
+	  htim11.Init.Period = 84-1;
+	  if (HAL_TIM_Base_Init(&htim11) != HAL_OK)
+	  {
+	    Error_Handler();
+	  }
+	  // Deshabilitar la interrupción de desbordamiento en TIM11 usando HAL
+	  __HAL_TIM_DISABLE_IT(&htim11, TIM_IT_UPDATE);
+	  // Borrar la bandera de desbordamiento en TIM11 usando HAL
+	  __HAL_TIM_CLEAR_FLAG(&htim11, TIM_FLAG_UPDATE);
+	  __HAL_TIM_SET_COUNTER(&htim11, 0);
 
 	osEventFlagsClear(flag_Event, 0x01); // Iniciar valor de flag
 
@@ -363,6 +511,22 @@ void delay_f (uint32_t uSeg){
 	osEventFlagsClear(flag_Event, 0x01);
 }
 
+void Delay_us(uint32_t us)
+{
+    // Convertir microsegundos a milisegundos
+    uint32_t ms = (us + 999) / 1000;
+
+    // Esperar en milisegundos
+    vTaskDelay(ms);
+}
+
+void delayWithNop(uint32_t delayTime) {
+	delayTime = delayTime*7;
+
+	for (uint32_t i = 0; i < delayTime; ++i) {
+        __NOP();
+    }
+}
 
 /* USER CODE END 4 */
 
@@ -394,50 +558,14 @@ void StartDefaultTask(void *argument)
 void StartTask02(void *argument)
 {
   /* USER CODE BEGIN StartTask02 */
-	//uint8_t tx_buffer [27] = "Hola mndo\n\r";
-	uint8_t buff_erre[2] = "XX";
-	uint8_t humidity, temperature;
-	uint8_t buff_print[24] ;
-	flag_Event = osEventFlagsNew(NULL);
-
-	// Crear variable y cargar valores inicialies de puerto
-	DHT11_TypeDef dht11;
-	dht11.GPIO_Port = DHT11_GPIO_Port;  // Reemplaza 'x' con el puerto GPIO que estás utilizando
-	dht11.GPIO_Pin = DHT11_Pin;  // Reemplaza 'x' con el número del pin GPIO que estás utilizando
-	DHT11_Init(&dht11);
-
-	osDelay(1000);
-
   /* Infinite loop */
+	uint8_t bufferEnvio[5] = "HOLA";
+	uint16_t tam = 5;
+	__HAL_UART_ENABLE_IT(&huart1, UART_IT_TC);
   for(;;)
   {
-	  /*osEventFlagsWait(flag_Event, 0x01, osFlagsWaitAll, osWaitForever);
-	  osEventFlagsClear(flag_Event, 0x01);
-	  HAL_UART_Transmit(&huart2, tx_buffer, 27, 10U);
-	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-	   */
-
-	  //HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-
-
-	 HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
-	 osDelay(1000);
-	 HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-	 osDelay(1000);
-	 if (DHT11_ReadData(&dht11, &humidity, &temperature) == HAL_OK) {
-		// Los datos se leyeron correctamente
-		// Utiliza las variables 'humidity' y 'temperature' según tus necesidades
-
- 		  formatSensorData(buff_print,  humidity,  temperature);
-
-		  HAL_UART_Transmit(&huart2, buff_print, 23, 10U);
-	  } else {
-		// Hubo un error al leer los datos del sensor
-		  HAL_UART_Transmit(&huart2, buff_erre, 2, 10U);
-	  }
-
-	  osDelay(1);
-
+	  HAL_UART_Transmit_IT(&huart1, bufferEnvio, tam);
+    osDelay(1000);
   }
   /* USER CODE END StartTask02 */
 }
@@ -453,11 +581,196 @@ void StartTask03(void *argument)
 {
   /* USER CODE BEGIN StartTask03 */
   /* Infinite loop */
+	__HAL_UART_ENABLE_IT(&huart1, UART_IT_RXNE);
+	uint8_t rx_buff [1] ;
+	uint16_t tam = 1;
   for(;;)
   {
-    osDelay(1);
+	  HAL_UART_Receive_IT(&huart1, rx_buff, tam);
+     osDelay(1000);
   }
   /* USER CODE END StartTask03 */
+}
+
+/* USER CODE BEGIN Header_StartTask04 */
+/**
+* @brief Function implementing the myTask04 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask04 */
+void StartTask04(void *argument)
+{
+  /* USER CODE BEGIN StartTask04 */
+  /* Infinite loop */
+	uint8_t buff_erre[2] = "XX";
+		uint8_t humidity, temperature;
+		uint8_t buff_print[24] ;
+		flag_Event = osEventFlagsNew(NULL);
+
+		// Crear variable y cargar valores inicialies de puerto
+		DHT11_TypeDef dht11;
+		dht11.GPIO_Port = DHT11_GPIO_Port;  // Reemplaza 'x' con el puerto GPIO que estás utilizando
+		dht11.GPIO_Pin = DHT11_Pin;  // Reemplaza 'x' con el número del pin GPIO que estás utilizando
+		DHT11_Init(&dht11);
+
+		osDelay(1000);
+  for(;;)
+  {
+	  HAL_UART_Transmit(&huart2, buff_print, 23, 10U);
+	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+
+	  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+	  	 osDelay(1000);
+	  	 HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+	  	 osDelay(1000);
+	  	 if (DHT11_ReadData_prueba(&dht11, &humidity, &temperature) == HAL_OK) {
+	  		// Los datos se leyeron correctamente
+	  		// Utiliza las variables 'humidity' y 'temperature' según tus necesidades
+
+	   		  formatSensorData(buff_print,  humidity,  temperature);
+
+	  		  HAL_UART_Transmit(&huart2, buff_print, 23, 10U);
+	  	  } else {
+	  		// Hubo un error al leer los datos del sensor
+	  		HAL_UART_Transmit(&huart2, buff_erre, 2, 10U);
+	  	  }
+
+    osDelay(1);
+  }
+  /* USER CODE END StartTask04 */
+}
+
+/* USER CODE BEGIN Header_StartTask05 */
+/**
+* @brief Function implementing the myTask05 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask05 */
+void StartTask05(void *argument)
+{
+  /* USER CODE BEGIN StartTask05 */
+  /* Infinite loop */
+  for(;;)
+  {
+
+	  if (mode == 0){
+	  		  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	  		  osDelay(100);
+	  	  }
+	  	  if (mode == 1){
+	  		  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	  		  		  osDelay(1000);
+	  	  }
+	  	  if (mode == 2){
+	  		  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	  		  osDelay(2000);
+	  	  }
+
+  }
+  /* USER CODE END StartTask05 */
+}
+
+/* USER CODE BEGIN Header_StartTask06 */
+/**
+* @brief Function implementing the myTask06 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask06 */
+void StartTask06(void *argument)
+{
+  /* USER CODE BEGIN StartTask06 */
+
+
+  /* Infinite loop */
+
+	  char uart_buf[50];
+	  int uart_buf_len;
+	  uint16_t timer_val;
+	  uint16_t timer_val2;
+	  // Say something
+	    uart_buf_len = sprintf(uart_buf, "Timer Test\r\n");
+	    HAL_UART_Transmit(&huart2, (uint8_t *)uart_buf, uart_buf_len, 100);
+
+	    // Start timer
+	    HAL_TIM_Base_Start(&htim10);
+  for(;;)
+  {
+	  	  osDelay(1000);
+	  	HAL_TIM_Base_Start(&htim10);
+	  	  timer_val = __HAL_TIM_GET_COUNTER(&htim10);
+
+        // Wait for 50 ms
+        //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+    	//DHT11_Delay_f(100);
+    	delayWithNop(7*20);
+    	//osDelay(1);
+    	//Delay_us(1);
+        //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+        // Get time elapsed
+        timer_val = __HAL_TIM_GET_COUNTER(&htim10) - timer_val;
+        timer_val2 = __HAL_TIM_GET_COUNTER(&htim11);
+        // Show elapsed time
+        HAL_TIM_Base_Stop_IT(&htim10);
+        uart_buf_len = sprintf(uart_buf, "%u us\r\n", timer_val);
+        HAL_UART_Transmit(&huart2, (uint8_t *)uart_buf, uart_buf_len, 100);
+        uart_buf_len = sprintf(uart_buf, "CNT:%u | ARR 83\r\n\n", timer_val2);
+        HAL_UART_Transmit(&huart2, (uint8_t *)uart_buf, uart_buf_len, 100);
+  }
+
+  /* USER CODE END StartTask06 */
+}
+
+/* USER CODE BEGIN Header_StartTask07 */
+/**
+* @brief Function implementing the myTask07 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask07 */
+void StartTask07(void *argument)
+{
+  /* USER CODE BEGIN StartTask07 */
+  /* Infinite loop */
+	/*
+	char uart_buf[50];
+		  int uart_buf_len;
+		  uint16_t timer_val;
+		  uint16_t timer_val2;
+		  // Say something
+		    uart_buf_len = sprintf(uart_buf, "Timer Test\r\n");
+		    HAL_UART_Transmit(&huart2, (uint8_t *)uart_buf, uart_buf_len, 100);
+
+		    // Start timer
+		    HAL_TIM_Base_Start(&htim10);
+
+	InitDelayTimer();
+  for(;;)
+  {
+
+
+	  timer_val = __HAL_TIM_GET_COUNTER(&htim10);
+
+	          // Wait for 50 ms
+	          //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	  Delay_us(10000);
+	          //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+	          // Get time elapsed
+	          timer_val = __HAL_TIM_GET_COUNTER(&htim10) - timer_val;
+	          timer_val2 = __HAL_TIM_GET_COUNTER(&htim11);
+	          // Show elapsed time
+	          uart_buf_len = sprintf(uart_buf, "%u us\r\n", timer_val);
+	          HAL_UART_Transmit(&huart2, (uint8_t *)uart_buf, uart_buf_len, 100);
+	          uart_buf_len = sprintf(uart_buf, "CNT:%u | ARR 83\r\n\n", timer_val2);
+	          HAL_UART_Transmit(&huart2, (uint8_t *)uart_buf, uart_buf_len, 100);*/
+	for(;;)
+	  {osDelay(1000);
+  }
+  /* USER CODE END StartTask07 */
 }
 
 /**
